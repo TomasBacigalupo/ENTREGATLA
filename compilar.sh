d8b645f392d2cb043ed617cc47ffe499e85e54d7
gmarchetti96 <gmarchetti@itba.edu.ar>
2019-12-02 01:09:17 -0300

    compilar ok

--- compilar.sh
+++ compilar.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# check if the source file exists
 FILE_SOURCE="$1"
-if [ ! ${FILE_SOURCE: -5} == ".ag" ]; then
-    echo "El archivo debe ser de la extension .ag"
-    exit 1
-fi
-
-# check if the number of arguments are correct
-if [ "$#" -ne 2 ]; then
-    echo ""
-    exit 1
-fi
-
 
 # generate the code
-cd src
-./compilar < ${FILE_SOURCE} > out.c
-cd ..
+./src/compiler < ${FILE_SOURCE} > out.c
+gcc -o ${FILE_SOURCE}.out out.c
+##rm -rf out.c
